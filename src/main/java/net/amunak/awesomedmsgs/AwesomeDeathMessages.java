dbdc4f0b759a46b6e8ebc30c122a2f50475a138e
Amunak <git@amunak.net>
2014-10-14 04:13:33 +0200

    fixed encoding issues

--- src/main/java/net/amunak/awesomedmsgs/AwesomeDeathMessages.java
+++ src/main/java/net/amunak/awesomedmsgs/AwesomeDeathMessages.java
@@ -1,7 +1,7 @@
 package net.amunak.awesomedmsgs;
 
 /**
-* Copyright 2014 Ji¯Ì Barouö
+* Copyright 2014 Ji≈ô√≠ Barou≈°
 *
 * This program is free software: you can redistribute it and/or modify it under
 * the terms of the GNU General Public License as published by the Free Software
